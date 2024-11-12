3b3fa358d6b624c4a218d67d86aeac5346d6eabf
Rob Lourens <roblourens@gmail.com>
2024-11-12 11:05:40 -0800

    Add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,86 @@
+// @ts-check
+
+import tseslint from 'typescript-eslint';
+import header from 'eslint-plugin-header';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+
+// https://github.com/Stuk/eslint-plugin-header/issues/57
+header.rules.header.meta.schema = false;
+
+export default tseslint.config({
+	files: ['src/**/*.ts*'],
+	ignores: ['src/util/vs/**'],
+	plugins: {
+		'@typescript-eslint': tseslint.plugin,
+		header: header,
+		'@stylistic/ts': stylisticTs,
+	},
+	languageOptions: {
+		parser: tseslint.parser,
+		parserOptions: {
+			projectService: true,
+			tsconfigRootDir: import.meta.dirname,
+		},
+	},
+	rules: {
+		indent: [
+			'error',
+			'tab',
+			{
+				ignoredNodes: [
+					'SwitchCase',
+					'ClassDeclaration',
+					'TemplateLiteral *', // Conflicts with tsfmt
+				],
+			},
+		],
+		'constructor-super': 'error',
+		curly: 'error',
+		eqeqeq: 'error',
+		'prefer-const': [
+			'error',
+			{
+				destructuring: 'all',
+			},
+		],
+		'no-buffer-constructor': 'error',
+		'no-caller': 'error',
+		'no-case-declarations': 'error',
+		'no-debugger': 'error',
+		'no-duplicate-case': 'error',
+		'no-duplicate-imports': 'error',
+		'no-eval': 'error',
+		'no-async-promise-executor': 'error',
+		'no-extra-semi': 'error',
+		'no-new-wrappers': 'error',
+		'no-redeclare': 'off',
+		'no-sparse-arrays': 'error',
+		'no-throw-literal': 'error',
+		'no-unsafe-finally': 'error',
+		'no-unused-labels': 'error',
+		'no-restricted-globals': [
+			'error',
+			'name',
+			'length',
+			'event',
+			'closed',
+			'external',
+			'status',
+			'origin',
+			'orientation',
+			'context',
+		], // non-complete list of globals that are easy to access unintentionally
+		'no-var': 'error',
+		'@stylistic/ts/semi': 'error',
+		'@stylistic/ts/member-delimiter-style': 'error',
+		'header/header': [
+			'error',
+			'block',
+			[
+				'---------------------------------------------------------------------------------------------',
+				' *  Copyright (c) Microsoft Corporation and GitHub. All rights reserved.',
+				' *--------------------------------------------------------------------------------------------',
+			],
+		],
+	},
+});
--- src/chatParticipantHandler.ts
+++ src/chatParticipantHandler.ts
@@ -44,7 +44,7 @@ export interface ChatHandlerOptions {
         stream: vscode.ChatResponseStream;
         references?: boolean;
         responseText?: boolean;
-    }
+    };
 }
 
 export interface ChatHandlerResult {
@@ -56,125 +56,125 @@ export interface ChatHandlerResult {
  * Send a chat request, do the tool calling loop if needed, and return a stream and a ChatResult. Caller handles the response stream.
  */
 export function sendChatParticipantRequest(request: vscode.ChatRequest, context: vscode.ChatContext, options: ChatHandlerOptions, token: vscode.CancellationToken): ChatHandlerResult {
-    const stream = new AsyncIterableSource<vscode.LanguageModelTextPart | vscode.LanguageModelToolResult>();
-    const resultPromise = _sendChatParticipantRequest(stream, request, context, options, token);
-    return {
-        result: resultPromise,
-        stream: stream.asyncIterable,
-    };
+	const stream = new AsyncIterableSource<vscode.LanguageModelTextPart | vscode.LanguageModelToolResult>();
+	const resultPromise = _sendChatParticipantRequest(stream, request, context, options, token);
+	return {
+		result: resultPromise,
+		stream: stream.asyncIterable,
+	};
 }
 
 async function _sendChatParticipantRequest(stream: AsyncIterableSource<vscode.LanguageModelTextPart | vscode.LanguageModelToolResult>, request: vscode.ChatRequest, context: vscode.ChatContext, options: ChatHandlerOptions, token: vscode.CancellationToken): Promise<vscode.ChatResult> {
-    let model = options.model ?? request.model;
-    if (options.tools?.length && model.vendor === 'copilot' && model.family.startsWith('o1')) {
-        // The o1 models do not currently support tools
-        const models = await vscode.lm.selectChatModels({
-            vendor: 'copilot',
-            family: 'gpt-4o'
-        });
-        model = models[0];
-    }
+	let model = options.model ?? request.model;
+	if (options.tools?.length && model.vendor === 'copilot' && model.family.startsWith('o1')) {
+		// The o1 models do not currently support tools
+		const models = await vscode.lm.selectChatModels({
+			vendor: 'copilot',
+			family: 'gpt-4o'
+		});
+		model = models[0];
+	}
 
-    // Use all tools, or tools with the tags that are relevant.
-    const tools = options.tools;
-    const requestOptions: vscode.LanguageModelChatRequestOptions = {
-        justification: options.requestJustification,
-    };
+	// Use all tools, or tools with the tags that are relevant.
+	const tools = options.tools;
+	const requestOptions: vscode.LanguageModelChatRequestOptions = {
+		justification: options.requestJustification,
+	};
 
-    // Render the initial prompt
-    const result = await renderPrompt(
-        ToolUserPrompt,
-        {
-            context,
-            request,
-            toolCallRounds: [],
-            toolCallResults: {},
-            libUserPrompt: options.prompt
-        },
-        { modelMaxPromptTokens: model.maxInputTokens },
-        model);
-    let messages = result.messages;
-    result.references.forEach(ref => {
-        if (ref.anchor instanceof vscode.Uri || ref.anchor instanceof vscode.Location) {
-            if (options.responseStreamOptions?.references) {
-                options.responseStreamOptions?.stream.reference(ref.anchor);
-            }
-        }
-    });
+	// Render the initial prompt
+	const result = await renderPrompt(
+		ToolUserPrompt,
+		{
+			context,
+			request,
+			toolCallRounds: [],
+			toolCallResults: {},
+			libUserPrompt: options.prompt
+		},
+		{ modelMaxPromptTokens: model.maxInputTokens },
+		model);
+	let messages = result.messages;
+	result.references.forEach(ref => {
+		if (ref.anchor instanceof vscode.Uri || ref.anchor instanceof vscode.Location) {
+			if (options.responseStreamOptions?.references) {
+				options.responseStreamOptions?.stream.reference(ref.anchor);
+			}
+		}
+	});
 
-    const toolReferences = [...request.toolReferences];
-    const accumulatedToolResults: Record<string, vscode.LanguageModelToolResult> = {};
-    const toolCallRounds: ToolCallRound[] = [];
-    const runWithTools = async (): Promise<void> => {
-        // If a toolReference is present, force the model to call that tool
-        const requestedTool = toolReferences.shift();
-        if (requestedTool) {
-            requestOptions.toolMode = vscode.LanguageModelChatToolMode.Required;
-            requestOptions.tools = vscode.lm.tools.filter(tool => tool.name === requestedTool.name);
-        } else {
-            requestOptions.toolMode = undefined;
-            requestOptions.tools = tools ? [...tools] : undefined;
-        }
+	const toolReferences = [...request.toolReferences];
+	const accumulatedToolResults: Record<string, vscode.LanguageModelToolResult> = {};
+	const toolCallRounds: ToolCallRound[] = [];
+	const runWithTools = async (): Promise<void> => {
+		// If a toolReference is present, force the model to call that tool
+		const requestedTool = toolReferences.shift();
+		if (requestedTool) {
+			requestOptions.toolMode = vscode.LanguageModelChatToolMode.Required;
+			requestOptions.tools = vscode.lm.tools.filter(tool => tool.name === requestedTool.name);
+		} else {
+			requestOptions.toolMode = undefined;
+			requestOptions.tools = tools ? [...tools] : undefined;
+		}
 
-        // Send the request to the LanguageModelChat
-        const response = await model.sendRequest(messages, requestOptions, token);
+		// Send the request to the LanguageModelChat
+		const response = await model.sendRequest(messages, requestOptions, token);
 
-        // Stream text output and collect tool calls from the response
-        const toolCalls: vscode.LanguageModelToolCallPart[] = [];
-        let responseStr = '';
-        for await (const part of response.stream) {
-            if (part instanceof vscode.LanguageModelTextPart) {
-                stream.emitOne(part);
-                responseStr += part.value;
+		// Stream text output and collect tool calls from the response
+		const toolCalls: vscode.LanguageModelToolCallPart[] = [];
+		let responseStr = '';
+		for await (const part of response.stream) {
+			if (part instanceof vscode.LanguageModelTextPart) {
+				stream.emitOne(part);
+				responseStr += part.value;
 
-                if (options.responseStreamOptions?.responseText) {
-                    options.responseStreamOptions.stream.markdown(part.value);
-                }
-            } else if (part instanceof vscode.LanguageModelToolCallPart) {
-                toolCalls.push(part);
-            }
-        }
+				if (options.responseStreamOptions?.responseText) {
+					options.responseStreamOptions.stream.markdown(part.value);
+				}
+			} else if (part instanceof vscode.LanguageModelToolCallPart) {
+				toolCalls.push(part);
+			}
+		}
 
-        if (toolCalls.length) {
-            // If the model called any tools, then we do another round- render the prompt with those tool calls (rendering the PromptElements will invoke the tools)
-            // and include the tool results in the prompt for the next request.
-            toolCallRounds.push({
-                response: responseStr,
-                toolCalls
-            });
-            const result = (await renderPrompt(
-                ToolUserPrompt,
-                {
-                    context,
-                    request,
-                    toolCallRounds,
-                    toolCallResults: accumulatedToolResults,
-                    libUserPrompt: options.prompt
-                },
-                { modelMaxPromptTokens: model.maxInputTokens },
-                model));
-            messages = result.messages;
-            const toolResultMetadata = result.metadatas.getAll(ToolResultMetadata);
-            if (toolResultMetadata?.length) {
-                // Cache tool results for later, so they can be incorporated into later prompts without calling the tool again
-                toolResultMetadata.forEach(meta => accumulatedToolResults[meta.toolCallId] = meta.result);
-            }
+		if (toolCalls.length) {
+			// If the model called any tools, then we do another round- render the prompt with those tool calls (rendering the PromptElements will invoke the tools)
+			// and include the tool results in the prompt for the next request.
+			toolCallRounds.push({
+				response: responseStr,
+				toolCalls
+			});
+			const result = (await renderPrompt(
+				ToolUserPrompt,
+				{
+					context,
+					request,
+					toolCallRounds,
+					toolCallResults: accumulatedToolResults,
+					libUserPrompt: options.prompt
+				},
+				{ modelMaxPromptTokens: model.maxInputTokens },
+				model));
+			messages = result.messages;
+			const toolResultMetadata = result.metadatas.getAll(ToolResultMetadata);
+			if (toolResultMetadata?.length) {
+				// Cache tool results for later, so they can be incorporated into later prompts without calling the tool again
+				toolResultMetadata.forEach(meta => accumulatedToolResults[meta.toolCallId] = meta.result);
+			}
 
-            // This loops until the model doesn't want to call any more tools, then the request is done.
-            return runWithTools();
-        }
-    };
+			// This loops until the model doesn't want to call any more tools, then the request is done.
+			return runWithTools();
+		}
+	};
 
-    await runWithTools();
+	await runWithTools();
 
-    stream.resolve();
-    return {
-        metadata: {
-            // Return tool call metadata so it can be used in prompt history on the next request
-            toolCallsMetadata: {
-                toolCallResults: accumulatedToolResults,
-                toolCallRounds
-            }
-        } satisfies TsxToolUserMetadata,
-    };
+	stream.resolve();
+	return {
+		metadata: {
+			// Return tool call metadata so it can be used in prompt history on the next request
+			toolCallsMetadata: {
+				toolCallResults: accumulatedToolResults,
+				toolCallRounds
+			}
+		} satisfies TsxToolUserMetadata,
+	};
 }
