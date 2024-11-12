f7961920b1dd470de904c0122f31d80d76d96e89
Rob Lourens <roblourens@gmail.com>
2024-11-12 11:16:31 -0800

    Setup tests

---
+++ .vscode-test.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from '@vscode/test-cli';
+
+export default defineConfig({
+	files: 'dist/**/*.test.js',
+	version: 'insiders',
+	launchArgs: ['--disable-extensions', '--profile-temp'],
+	mocha: {
+		ui: 'tdd',
+		color: true,
+		forbidOnly: !!process.env.CI,
+		timeout: 5000,
+	},
+});
---
+++ src/test/handler.test.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation and GitHub. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as assert from 'assert';
+
+suite('placeholder test', () => {
+	test('should return true', () => {
+		const result = true;
+		assert.ok(result);
+	});
+});
